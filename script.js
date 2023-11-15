97561f3368a5a5281fc278414cacebaf02129ddb
chekurihemanth <96361549+chekurihemanth@users.noreply.github.com>
2023-11-15 10:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ function firstNonRepeatedChar(str) {
 		return null;
 	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+const charCount = new Array(26).fill(0);
+for (let index = 0; index < str.length; index++) {
+	const char = str[i];
+	charCount[char.charCodeAt(0)-'a'.charCodeAt(0)]++;
+	
+}
+for (let index = 0; index < str.length; index++) {
+		const char = str[i];
+	    if (charCount[char.charCodeAt(0)-'a'.charCodeAt(0)]===1) {
+			return char;
+	    	
+	    }
+}
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
